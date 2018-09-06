46d8f1214a22907ae3b2301e7dca49eaa1b2b487
nathaniel-wendt <nathaniel.wendt@gmail.com>
2018-09-06 14:30:02 -0600

    feat: wrap function get(url) in promise

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -33,26 +33,29 @@ Instructions:
    * @return {Promise}    - A Promise that resolves when the XHR succeeds and fails otherwise.
    */
   function get(url) {
-    /*
-    This code needs to get wrapped in a Promise!
-     */
+  
+  return new Promise(function(resolve, reject) {
     var req = new XMLHttpRequest();
     req.open('GET', url);
     req.onload = function() {
       if (req.status === 200) {
         // It worked!
         // You'll want to resolve with the data from req.response
+        resolve(req.response);
       } else {
         // It failed :(
         // Be nice and reject with req.statusText
-      }
+        reject(Error(req.statusText));
+      };
     };
     req.onerror = function() {
       // It failed :(
       // Pass a 'Network Error' to reject
+      reject(Error('Network Error'));
     };
     req.send();
-  }
+    });
+  };
 
   window.addEventListener('WebComponentsReady', function() {
     home = document.querySelector('section[data-route="home"]');
